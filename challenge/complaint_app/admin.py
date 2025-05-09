1aca4019b264422a24735d3f1cd9989c0cc07547
Sebastian Vivas <seb.vivas.dev@gmail.com>
2025-05-09 13:56:44 -0400

    Add README

--- challenge/complaint_app/admin.py
+++ challenge/complaint_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from complaint_app.models import UserProfile, Complaint
 
-# Register your models here.
 admin.site.register(Complaint)
 admin.site.register(UserProfile)
--- challenge/complaint_app/models.py
+++ challenge/complaint_app/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
-# Create your models here.
 class UserProfile(models.Model):
   id = models.BigAutoField(primary_key=True)
   user = models.OneToOneField(User, on_delete=models.CASCADE)
